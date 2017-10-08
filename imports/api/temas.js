0ff6aa6d9878907bf6a2d7761592d765730d1db3
hjcalderon10 <hj.calderon10@uniandes.edu.co>
2017-10-08 17:09:50 -0500

    remove insecure, autopublish. add check and methods

--- imports/api/temas.js
+++ imports/api/temas.js
@@ -1,3 +1,19 @@
-import {Mongo} from "meteor/mongo";
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
 
 export const Temas = new Mongo.Collection("Temas"); 
+
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish('temas', function temasPublication() {
+    return Temas.find();
+  });
+}
+
+
+Meteor.methods({
+
+
+
+});
