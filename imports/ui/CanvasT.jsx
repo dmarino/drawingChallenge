29550f5ae7979f27bd92b7bd1839aaf521f27c6e
RUTH CECILIA RODRIGUEZ BUITRAGO <d.marino10@uniandes.edu.co>
2017-10-08 14:29:19 -0500

    canvasT ya se ve bien :3

--- imports/ui/CanvasT.jsx
+++ imports/ui/CanvasT.jsx
@@ -9,6 +9,10 @@ class CanvasT extends Component{
 
 	constructor(props){
 		super(props);
+		this.state = {
+		    likes:0,
+		    cambio:true
+		}
 	}
 
 	componentDidMount(){
@@ -16,6 +20,14 @@ class CanvasT extends Component{
 	}
 
 	componentWillUpdate(){
+	    if(this.state.cambio){
+	        var num = this.props.dibujo.likes+1;
+
+	        this.setState({
+	            likes: num,
+	            cambio: false
+	        });
+	    }
 		this.redraw();
 	}
 
@@ -24,12 +36,18 @@ class CanvasT extends Component{
 	    Dibujos.update(this.props.dibujo._id, {
             $set: { likes:num},
         });
+
         this.forceUpdate();
+
+	    this.setState({
+	        likes: num,
+	        cambio: true
+	    })        
 	}
 
 	redraw() {
         let ctx = this.canvas.getContext("2d");
-        ctx.clearRect(0, 0, 500, 500);
+        ctx.clearRect(0, 0,500, 500);
 
         for(var i=0; i < this.props.dibujo.dibujo.length; i++) {  
             ctx.beginPath();
@@ -49,14 +67,16 @@ class CanvasT extends Component{
 		return (
 		    <div className="CanvasT">
 			    <canvas 
+                    width="500" 
+                    height="500"
 				    ref={(c)=>this.canvas=c}
 			    >
 			    </canvas>
                 <div className="container">
 			        {this.props.tema ?
-					    <h4><b>{this.props.dibujo.concurso}</b></h4> 
+					    <h5><b>{this.props.dibujo.concurso}</b></h5> 
 				    :
-					    <h4><b>{this.props.dibujo.autor}</b></h4> 				
+					    <h5><b>{this.props.dibujo.autor}</b></h5> 				
 				    }            
                     <p>likes: {this.props.dibujo.likes}</p> 
                     {this.props.like ?
